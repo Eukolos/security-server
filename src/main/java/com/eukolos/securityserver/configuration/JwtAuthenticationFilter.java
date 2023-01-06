f63c04d1d4ca21dad77c8dbe5cb85eaea174877e
Eukolos <eminaksoy35@gmail.com>
2023-01-06 23:16:50 +0300

    injected jwt depends, created jwt service

--- src/main/java/com/eukolos/securityserver/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/eukolos/securityserver/configuration/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -21,11 +22,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request,response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail =  jwtService.extractUsername(jwt);
 
 
     }
---
+++ src/main/java/com/eukolos/securityserver/configuration/JwtService.java
@@ -0,0 +1,11 @@
+package com.eukolos.securityserver.configuration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+
+        return null;//50
+    }
+}
